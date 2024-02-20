b32ac23055c5a1d2a02bb710f2e414858a4c20b2
Chris Gresty <chris@gresty.dev>
2024-02-20 13:20:43 +0000

    Upgrade to 2.8.3

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export IMAGE=registry
-export IMAGE_VERSION=2.8.1
+export IMAGE_VERSION=2.8.3
 export SUFFIX=2tudor
 export REGISTRY=docker.2tudor.net
 export CA_ROOT=root_ca-2tudor.net.crt
